67ecc3507d20ee59474b2bc4166af3fef1e41dd6
egorsobolev2000 <web-disain@mail.ua>
2021-07-29 15:04:30 +0300

    Склеил шейдеры и текстуру

--- src/engine.cpp
+++ src/engine.cpp
@@ -6,6 +6,8 @@
 #include <iostream>
 
 #include "graphics/Shader.h"
+#include "graphics/Texture.h"
+#include "loaders/PNGLoading.h"
 #include "window/Events.h"
 #include "window/Window.h"
 
@@ -13,11 +15,12 @@ int WIDTH = 1280;
 int HEIGHT = 720;
 
 float vertices[] = {
-    0.0f, 0.0f, 0.0f,
+    // x    y     z     u     v
+    -1.0f, -1.0f, 0.0f,  0.0f, 0.0f, 1.0f, -1.0f, 0.0f,
+    1.0f,  0.0f,  -1.0f, 1.0f, 0.0f, 0.0f, 1.0f,
 
-    1.0f, 0.0f, 0.0f,
-
-    0.0f, 1.0f, 0.0f,
+    1.0f,  -1.0f, 0.0f,  1.0f, 0.0f, 1.0f, 1.0f,  0.0f,
+    1.0f,  1.0f,  -1.0f, 1.0f, 0.0f, 0.0f, 1.0f,
 };
 
 int main(int argc, char const* argv[]) {
@@ -26,11 +29,21 @@ int main(int argc, char const* argv[]) {
   // инициализация системы Events
   Events::initialize();
 
+  // загрузка шейдеров (тест)
   Shader* shader = LoadShader("res/main.glslv", "res/main.glslf");
   if (shader == nullptr) {
     std::cerr << "Неудалось загрузить шейдеры" << std::endl;
     Window::terminate();
-    return -1;
+    return 1;
+  }
+
+  // загрузка текстуры (тест)
+  Texture* texture = LoadTexture("res/img.png");
+  if (texture == nullptr) {
+    std::cerr << "Ошибка загрузки текстуры" << std::endl;
+    delete shader;
+    Window::terminate();
+    return 1;
   }
 
   // создаю объект вершинных массивов
@@ -43,13 +56,22 @@ int main(int argc, char const* argv[]) {
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
   // (index, длинна вершинного атрибута, тип данных, автоматическая
   // нормализация, длинна шака вершинных данных, сдвиг вершинного атрибутта)
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat),
                         (GLvoid*)(0 * sizeof(GLfloat)));
-
   glEnableVertexAttribArray(0);  // включение вершинного атрибута
+
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat),
+                        (GLvoid*)(3 * sizeof(GLfloat)));
+  glEnableVertexAttribArray(1);
+
   glBindVertexArray(0);  // отвязываю объект
 
-  glClearColor(0.207f, 0.59f, 0.99f, 1);
+  glClearColor(0.6f, 0.62f, 0.65f, 1);
+
+  // включаю смешивание для корректной прозрачности
+  glEnable(GL_CULL_FACE);
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
   while (!Window::isShouldClose()) {
     Events::pollEvents();
@@ -59,22 +81,29 @@ int main(int argc, char const* argv[]) {
 
     // тестирование обработки нажатия мышки
     if (Events::justClicked(GLFW_MOUSE_BUTTON_1)) {
-      glClearColor(0.100f, 0.43f, 0.100f, 1);
+      glClearColor(0.8f, 0.4f, 0.2f, 1);
     }
 
     glClear(GL_COLOR_BUFFER_BIT);
 
     // назначаю использование шейдера
     shader->use();
+    // привязываю текстуру
+    texture->bind();
+
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
     glBindVertexArray(0);
 
     Window::swapBuffers();
   }
 
   // Освобождение всех ресурсов
-  Window::terminate();
+  delete shader;
+  delete texture;
+  glDeleteBuffers(1, &VBO);
+  glDeleteVertexArrays(1, &VAO);
 
+  Window::terminate();
   return 0;
 }
