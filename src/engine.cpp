ea59bd41477700bbfb9feb74039d3fd0c34b15ee
egorsobolev2000 <web-disain@mail.ua>
2021-07-31 03:47:19 +0300

    Добавил и подключил камеру

--- src/engine.cpp
+++ src/engine.cpp
@@ -3,11 +3,15 @@
 
 #define GLEW_STATIC
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
 #include <iostream>
 
 #include "graphics/Shader.h"
 #include "graphics/Texture.h"
 #include "loaders/PNGLoading.h"
+#include "window/Camera.h"
 #include "window/Events.h"
 #include "window/Window.h"
 
@@ -69,25 +73,75 @@ int main(int argc, char const* argv[]) {
   glClearColor(0.6f, 0.62f, 0.65f, 1);
 
   // включаю смешивание для корректной прозрачности
-  glEnable(GL_CULL_FACE);
+  // glEnable(GL_CULL_FACE);
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+  // создаю камеру
+  Camera* camera = new Camera(glm::vec3(0, 0, 1), glm::radians(90.0f));
+
+  glm::mat4 model(1.0f);
+  model = glm::translate(model, glm::vec3(0.5f, 0, 0));
+
+  // дельта времени (время прошедшее с предыдущего кадра)
+  float lastTime = glfwGetTime();
+  float delta = 0.0f;
+
+  float camX = 0.0f;
+  float camY = 0.0f;
+
+  float speed = 5;
   while (!Window::isShouldClose()) {
-    Events::pollEvents();
+    // стабилизация FPS
+    float currentTime = glfwGetTime();
+    delta = currentTime - lastTime;
+    lastTime = currentTime;
     if (Events::justPressed(GLFW_KEY_ESCAPE)) {
       Window::setShouldClose(true);
     }
 
-    // тестирование обработки нажатия мышки
-    if (Events::justClicked(GLFW_MOUSE_BUTTON_1)) {
-      glClearColor(0.8f, 0.4f, 0.2f, 1);
+    if (Events::justPressed(GLFW_KEY_TAB)) {
+      Events::toogleCursor();
+    }
+
+    // управление камерой с клавиатуры
+    if (Events::isPressed(GLFW_KEY_W)) {
+      camera->position += camera->front * delta * speed;
+    }
+    if (Events::isPressed(GLFW_KEY_S)) {
+      camera->position -= camera->front * delta * speed;
+    }
+    if (Events::isPressed(GLFW_KEY_D)) {
+      camera->position += camera->right * delta * speed;
+    }
+    if (Events::isPressed(GLFW_KEY_A)) {
+      camera->position -= camera->right * delta * speed;
+    }
+
+    if (Events::_cursor_locked) {
+      camY += -Events::deltaY / Window::height * 2;
+      camX += -Events::deltaX / Window::height * 2;
+
+      if (camY < -glm::radians(89.0f)) {
+        camY = -glm::radians(89.0f);
+      }
+      if (camY > -glm::radians(89.0f)) {
+        camY = -glm::radians(89.0f);
+      }
+
+      camera->rotation = -glm::mat4(1.0f);
+      camera->rotate(camY, camX, 0);
     }
 
     glClear(GL_COLOR_BUFFER_BIT);
 
     // назначаю использование шейдера
     shader->use();
+    // передача матрицы в шейдер
+    shader->uniformMatrix("model", model);
+    // передача в шейдер projview
+    shader->uniformMatrix("projview",
+                          camera->getProjection() * camera->getView());
     // привязываю текстуру
     texture->bind();
 
@@ -96,6 +150,7 @@ int main(int argc, char const* argv[]) {
     glBindVertexArray(0);
 
     Window::swapBuffers();
+    Events::pollEvents();
   }
 
   // Освобождение всех ресурсов
