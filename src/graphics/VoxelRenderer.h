a6602ff045bb3ca08dc4ed33dc44123c06221a7e
egorsobolev2000 <web-disain@mail.ua>
2021-08-02 17:28:41 +0300

    Рендеринг вокселей

---
+++ src/graphics/VoxelRenderer.h
@@ -0,0 +1,20 @@
+#ifndef GRAPHICS_VOXELRENDERER_H
+#define GRAPHICS_VOXELRENDERER_H
+
+#include <stdlib.h>
+
+class Mesh;
+class Chunk;
+
+class VoxelRenderer {
+  float* buffer;
+  size_t capacity;
+
+ public:
+  VoxelRenderer(size_t capacity);
+  ~VoxelRenderer();
+
+  Mesh* render(Chunk* chunk, const Chunk** chunks);
+};
+
+#endif
